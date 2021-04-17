f637aebbbbae676bfe6a64723e78114fa15d1765
sohey-H <60923547+sohey-dr@users.noreply.github.com>
2021-04-17 09:38:31 +0900

    skeleton/section02/step01 完成

--- skeleton/section02/step01/main.go
+++ skeleton/section02/step01/main.go
@@ -15,8 +15,9 @@ func main() {
 	rand.Seed(time.Now().Unix())
 
 	// TODO: 長さ11の文字列型の配列を変数resultとして定義する
+	var result[11] string
 
-	for i := 0; /* TODO: 継続条件をiがresultの要素数より小さい場合にする */; i++ {
+	for i := 0; i < len(result) ; i++ {
 
 		// 0から99までの間で乱数を生成する
 		num := rand.Intn(100)
