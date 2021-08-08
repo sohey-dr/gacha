0d8e4e847dae76c9c1e688b2e5439d6bbfcb5e92
soheyHashimoto <heyso1209@gmail.com>
2021-08-09 01:17:10 +0900

    [MOVE]rarityやcardに関する処理をcard.goに移す

--- skeleton/section04/step01/main.go
+++ skeleton/section04/step01/main.go
@@ -8,34 +8,6 @@ import (
 	"time"
 )
 
-// -- card.goに移す ここから --
-type rarity string
-
-const (
-	rarityN  rarity = "N"
-	rarityR  rarity = "R"
-	raritySR rarity = "SR"
-	rarityXR rarity = "XR"
-)
-
-func (r rarity) String() string {
-	return string(r)
-}
-
-type card struct {
-	rarity rarity // レア度
-	name   string // 名前
-}
-
-func (c *card) String() string {
-	// TODO: レア度:名前のように文字列を作る
-	// 例："SR:ドラゴン"
-	return c.rarity.String() + ":" + c.name
-}
-
-// -- card.goに移す ここまで --
-
-
 // -- player.goに移す ここから --
 
 type player struct {
